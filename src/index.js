721f45bc0e659a844d40b5a19d323a1c13c017b5
Fin Reinhard <fin.reinhard@icloud.com>
2019-10-27 13:57:56 +0100

    fetch number and log it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import fetch from 'node-fetch';
 
-fetch('https://www.teamtrees.org/')
-  .then(console.log);
+const handleHTTPErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.status);
+  }
+
+  return response;
+};
+
+fetch(
+  'https://www.teamtrees.org/',
+  {
+    Accept: 'text/html',
+    "Accept-Encoding": 'gzip'
+  }
+)
+  .then(handleHTTPErrors)
+  .then(response => response.text())
+  .then((text) => {
+    const match = text.match(/<div id="totalTrees" class="counter" data-count="(\d*)">0<\/div>/);
+    if (match) {
+      console.log(match[1]);
+    }
+  })
+  .catch(console.log);
