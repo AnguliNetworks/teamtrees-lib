d7cc9d3d83091b51963a6c53b67f6c5e5a31d669
Fin Reinhard <fin.reinhard@icloud.com>
2019-10-27 14:19:15 +0100

    move fetch call to function

--- src/index.js
+++ src/index.js
@@ -15,11 +15,26 @@ fetch(
   },
 )
   .then(handleHTTPErrors)
-  .then((response) => response.text())
-  .then((text) => {
-    const match = text.match(/<div id="totalTrees" class="counter" data-count="(\d*)">0<\/div>/);
-    if (match) {
-      console.log(match[1]);
-    }
-  })
-  .catch(console.log);
+const getDonatedTrees = () => new Promise((
+  (resolve, reject) => fetch(
+    'https://www.teamtrees.org/',
+    {
+      Accept: 'text/html',
+    },
+  )
+    .then(handleHTTPErrors)
+    .then((response) => response.text())
+    .then((text) => {
+      const match = text.match(/<div id="totalTrees" class="counter" data-count="(\d*)">0<\/div>/);
+      if (!match) {
+        throw Error('No tree count found.');
+      }
+
+      resolve(match[1]);
+    })
+    .catch(reject)
+));
+
+getDonatedTrees()
+  .then(console.log)
+  .catch(console.error);
